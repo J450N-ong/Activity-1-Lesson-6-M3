eb71605ae3bf0428fc24c9da77efe2c4522b5d85
J450N-ong <odeleyejason@gmail.com>
2024-11-24 15:24:16 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+
+today = date.today()
+now1 = datetime.now()
+
+print("Today's date is ",today.day,today.month,today.year)
+print("\nCurrent date and time is ",now1)
